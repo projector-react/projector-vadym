9c78ca9ab35c53bd6fbdb746341858e1e687040d
Vadym <vadymshukr@gmail.com>
2022-06-27 12:29:17 +0300

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-// import { AxiosApiService } from "./apiService/api-service";
-// import { createAxiosInstance } from "./apiService/create-axios-instance";
-// import { AuthService } from "./Auth/auth-service";
 
 export const App: React.FC = () => {
-    // const apiService = new AxiosApiService(createAxiosInstance())
-    // const auth = new AuthService(apiService);
-    // auth.login({username: 'projector_user', password: 'projector'})
     return <h1>Projector Library</h1>
 }
